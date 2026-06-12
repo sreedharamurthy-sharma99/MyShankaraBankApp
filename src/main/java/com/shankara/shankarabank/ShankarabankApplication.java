b5c088ff817de185b13fc20b2ae779dcbb2a7727
sreedharamurthy-sharma99 <sreedharamurthy.sharma99@gmail.com>
2026-06-12 13:31:03 +0530

    Added Starting project from Spring.io

---
+++ src/main/java/com/shankara/shankarabank/ShankarabankApplication.java
@@ -0,0 +1,13 @@
+package com.shankara.shankarabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShankarabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShankarabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shankara/shankarabank/ShankarabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankara.shankarabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShankarabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
